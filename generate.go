9430866c1a2a4b305a3d41c2e29da5584a654b3e
davidlion <david.lion@yscope.com>
2023-05-31 13:07:31 -0400

    Improve cmake building; remove static libraries

--- generate.go
+++ generate.go
@@ -1,5 +1,5 @@
 //go:generate cmake -E env GOOS=${GOOS} GOARCH=${GOARCH} cmake -S cpp -B cpp/build
-//go:generate cmake --build cpp/build
-//go:generate cmake --install cpp/build --prefix lib
+//go:generate cmake --build cpp/build -j
+//go:generate cmake --install cpp/build --prefix .
 
 package ffi
